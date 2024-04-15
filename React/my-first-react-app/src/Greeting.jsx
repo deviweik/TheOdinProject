3ac0e0cd3f7dcb8c6f094dae1f152fe457cca955
Devin Weikert <weikert.devin@gmail.com>
2024-04-14 23:14:07 -0400

    React notes

---
+++ React/my-first-react-app/src/Greeting.jsx
@@ -0,0 +1,16 @@
+function Greeting() {
+  return (
+    <>
+      <h1>&quot;Hello, World!&quot;</h1>
+      <div>
+        <p>My favorite foods types are mexican, american, and chinese.</p>
+        <p>
+          My favorite dishes are penne vodka and lamb chops with mint
+          chimichurri.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Greeting;
